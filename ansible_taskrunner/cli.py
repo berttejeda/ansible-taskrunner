a4927901fbd9804883042c02624e3d5bedb05c74
Tejeda, Engelbert <etejeda@seic.com>
2019-08-05 14:00:13 -0400

    Bumped version to 1.0.1

--- ansible_taskrunner/cli.py
+++ ansible_taskrunner/cli.py
@@ -71,7 +71,7 @@ Ansible Taskrunner - ansible-playbook wrapper
 
 # Private variables
 __author__ = 'etejeda'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __program_name__ = 'tasks'
 __debug = False
 verbose = 0
